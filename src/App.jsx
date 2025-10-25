aeb88f226f510d4d4aeade58a2a9b33a5e84fada
DevBilal17 <muhammadbilalmb0312@gmail.com>
2025-10-25 10:10:40 +0500

    Navbar Footer Component layout structure done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Reserve from "./pages/Reserve/Reserve";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 const App = () => {
   return (
     <>
-     <Routes>
+      <Navbar/>
+      <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/reserve" element={<Reserve/>} />
-     </Routes>
+      </Routes>
+      <Footer/>
     </>
   );
 };
