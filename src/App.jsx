65b01fa738d40eb4b9a213e1a6e817b19d1b6e24
DevBilal17 <muhammadbilalmb0312@gmail.com>
2025-10-25 10:08:33 +0500

    Routing setup donw using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Reserve from "./pages/Reserve/Reserve";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+     <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/reserve" element={<Reserve/>} />
+     </Routes>
+    </>
   );
 };
 
